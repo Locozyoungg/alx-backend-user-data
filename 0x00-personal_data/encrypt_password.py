97d7efa72e534ed173b968434b8f4000e9a9587c
Locozyoungg <collaustine27@gmail.com>
2024-08-01 03:40:43 +0300

    encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,7 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a password matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -28,7 +28,9 @@ print(get_logger.__annotations__.get('return'))
 print("PII_FIELDS: {}".format(len(PII_FIELDS)))
 
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
