09281dcc6b667733e5ff0709fc08a211f07de8bf
Locozyoungg <collaustine27@gmail.com>
2024-08-07 01:54:22 +0300

    fixed

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,11 +1,14 @@
+
 #!/usr/bin/env python3
 """
 Route module for the API
 """
 from os import getenv
-from flask import Flask, jsonify
-from flask_cors import CORS
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,12 +16,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Not found handler"""
+def not_found(error) -> str:
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=int(port))
+    app.run(host=host, port=port)
